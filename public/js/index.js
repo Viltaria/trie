c665c69ae49a9b4b9b3b66e4bd585a022a23b00a
Tyler Chong <tylrchong@gmail.com>
2017-08-21 18:10:45 -1000

    Possible Combinations

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,8 @@ const app = new Moon({
     searchInfo: '',
     history: [],
     possibilities: {},
-    using: "Webster's Unabridged Dictionary"
+    possibilities_count: 0,
+    using: "Webster's_Unabridged_Dictionary.json"
   }
 });
 
@@ -58,7 +59,15 @@ $.getJSON('new_dictionary.json', success);
 $.getJSON('public/new_dictionary.json', success);
 
 function success( data ) {
-  const root = trie(data);
+  const root = trie( data );
+  $('#download').click(function () {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(root));
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.setAttribute("download", app.get('using'));
+    dlAnchorElem.click();
+  });
+
   $('#search').on('input', function ( e ) {
     app.set('search', e.target.value);
     app.set('searchInfo', search(root, e.target.value));
@@ -112,7 +121,9 @@ function trie_recurse( obj, key, val ) {
 function search( root, search ) {
   search = search.toLowerCase();
   app.set('possibilities', []);
-  if (search) return search_recurse(root, search, []);
+  if (search) {
+    return search_recurse(root, search, []);
+  }
 }
 
 /*
@@ -124,7 +135,8 @@ function search_recurse( obj, search, history ) {
       if (obj[search].value) {
         history.push(search.slice(0, 1));
         app.set('history', history);
-	app.set('possibilities', Object.keys(obj[search]).sort());
+	      app.set('possibilities', Object.keys(obj[search]).sort());
+        app.set('possibilities_count', possibility(obj[search]));
         return obj[search].value;
       }
     }
@@ -137,5 +149,23 @@ function search_recurse( obj, search, history ) {
   history.push(search.slice(0, 1));
   app.set('history', history);
   app.set('possibilities', Object.keys(obj[n]).sort());
+  app.set('possibilities_count', possibility(obj));
   return search_recurse( obj[n], search.slice(1, search.length), history );
+}
+
+/*
+  Get all Possibilities left in the trie from a node
+*/
+function possibility ( node ) {
+  const keys = Object.keys(node);
+  let count = 0;
+  for (let i = 0; i < keys.length; i++) {
+    let val = node[keys[i]];
+    if (val !== null && typeof val === 'object') {
+      count += possibility( val );
+    } else {
+      count += 1;
+    }
+  }
+  return count;
 }
